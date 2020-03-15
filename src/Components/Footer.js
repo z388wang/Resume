69ae3dcb74bcb299ee68ea6e5509c35e16016b93
root <root@MSI.localdomain>
2020-03-15 02:00:48 -0400

    basic modify

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Footer extends Component {
   render() {
 
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function (network) {
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
     }
@@ -12,23 +13,25 @@ class Footer extends Component {
     return (
       <footer>
 
-     <div className="row">
-        <div className="twelve columns">
-           <ul className="social-links">
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">
               {networks}
-           </ul>
+            </ul>
 
-           <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
-           </ul>
 
+
+          </div>
+          <div id="go-top"><a className="smoothscroll" title="Back to Top" href={this.props.goBackPage}><i className={this.props.iconType}></i></a></div>
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
-     </div>
-  </footer>
+      </footer>
     );
   }
 }
+
+Footer.propTypes = {
+  goBackPage: PropTypes.string,
+  iconType: PropTypes.string,
+};
 
 export default Footer;
