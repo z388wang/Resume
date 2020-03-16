86678f092bc00430b9f115135cb3dc73bfec425f
root <root@MSI.localdomain>
2020-03-16 01:18:49 -0400

    add new features to home page and resume page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,6 @@ class Header extends Component {
 
       if (this.props.data) {
          var name = this.props.data.name;
-         var occupation = this.props.data.occupation;
-         var description = this.props.data.description;
-         var city = this.props.data.address.city;
          var networks = this.props.data.social.map(function (network) {
             return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
          })
@@ -15,7 +12,6 @@ class Header extends Component {
 
       return (
          <header id="home">
-
             <nav id="nav-wrap">
 
                <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
@@ -26,7 +22,6 @@ class Header extends Component {
                   <li><a className="smoothscroll" href="#about">About</a></li>
                   <li><a className="smoothscroll" href="#resume">Resume</a></li>
                   <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                  <li><a className="smoothscroll" href="#contact">Contact</a></li>
                </ul>
 
             </nav>
@@ -34,7 +29,6 @@ class Header extends Component {
             <div className="row banner">
                <div className="banner-text">
                   <h1 className="responsive-headline">I'm {name}.</h1>
-                  <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
                   <hr />
                   <ul className="social">
                      {networks}
